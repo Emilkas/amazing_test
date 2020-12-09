fece3828127865b86c5447049b656e642193b4d7
Vanek <30772009+Emilkas@users.noreply.github.com>
2020-12-09 12:10:51 +0700

    site

---
+++ assets/js/enter.js
@@ -0,0 +1,5 @@
+function Reg() {  
+    document.getElementById("Reg").setAttribute("style", "display: none");  
+    document.getElementById("s-in").setAttribute("style", "display: none");          
+    document.getElementById("s-up").setAttribute("style", "display: block");        
+}
